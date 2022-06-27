5e181c26026782281c5ef4d759c3923aad8a051e
Isabelle Holman <108281002+isabellelholman@users.noreply.github.com>
2022-06-27 08:54:27 -0500

    Update streamlit_app.py
    I added a drink menu and added emojis to the food options

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,16 @@ streamlit.title('La Fea Bistro')
 streamlit.header('Dinner Menu')
 
 streamlit.header('Appetizers')
-streamlit.text('Speacialty Salad')
+streamlit.text('🥗 Speacialty Salad')
 
 streamlit.header('Entrees')
-streamlit.text('Chefs Choice')
+streamlit.text('🥣 Chefs Choice')
 
 streamlit.header('Desserts')
-streamlit.text('Chefs Choice')
+streamlit.text('🥭 Chefs Choice')
+
+streamlit.header('Drink Menu')
+streamlit.text('🍇 Chefs Choice Red Wine')
+streamlit.text('🍇 Chefs Choice White Wine')
+streamlit.text('🥃 Chefs Choice Bourbon')
+
