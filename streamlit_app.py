73459761c2760af4c12a227d4e2a2468e8487d59
Isabelle Holman <108281002+isabellelholman@users.noreply.github.com>
2022-06-27 08:57:47 -0500

    Update streamlit_app.py
    Added a txt file through Pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,6 @@ streamlit.text('🍇 Chefs Choice Red Wine')
 streamlit.text('🍇 Chefs Choice White Wine')
 streamlit.text('🥃 Chefs Choice Bourbon')
 
+import pandas 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
