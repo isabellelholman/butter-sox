9b47bce0a1190e9730e423e565b028aa026c7a5a
Isabelle Holman <108281002+isabellelholman@users.noreply.github.com>
2022-06-29 15:43:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,19 +4,19 @@ streamlit.title('La Fea Bistro')
 
 streamlit.header('Dinner Menu')
 
-streamlit.header('Appetizers')
+streamlit.subheader('Appetizers')
 streamlit.text('🧀 Charcuterie Board 🍓')
 
-streamlit.header('Entrees')
+streamlit.subheader('Entrees')
 streamlit.text('🥣 Chefs Choice 👨‍🍳')
 
-streamlit.header('Drink Menu')
+streamlit.subheader('Drink Menu')
 streamlit.text('🍇 Chefs Choice Red Wine')
 streamlit.text('🥂 Chefs Choice White Wine')
 streamlit.text('🍾 Chefs Choice Champagne')
 streamlit.text('🥃 Chefs Choice Bourbon')
 
-streamlit.header('Desserts')
+streamlit.header('Dessert Menu')
 streamlit.text('🍨 Chefs Choice')
 
 
