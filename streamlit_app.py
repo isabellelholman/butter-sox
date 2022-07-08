e9ae6408748804f4bd6eacbd836bd860ced274ce
Isabelle Holman <108281002+isabellelholman@users.noreply.github.com>
2022-07-08 07:44:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,5 @@ streamlit.header('Fruityvice Fruit Advice!')
 import requests
 
 fruityvice_repsonse = requests.get("https://fruityvice.com/api/fruit/kiwi") 
-fruityvice_normalized = json_normalize(fruityvice_repsonse.json()) 
+fruityvice_normalized = json_normalized(fruityvice_repsonse.json()) 
 streamlit.dataframe(fruityvice_normalized)
