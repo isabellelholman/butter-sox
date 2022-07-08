e04590096ec9670bd3a3ee675e711b39f24daef6
Isabelle Holman <108281002+isabellelholman@users.noreply.github.com>
2022-07-08 08:04:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests 
 
+import pandas
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.text(fruityvice_response.json())
